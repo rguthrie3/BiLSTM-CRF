fd61f0616d023e343eb5fa791b0762b35364e100
rguthrie3 <rguthrie3@gatech.edu>
2016-12-03 21:16:43 -0500

    Make dataset directly reads conll files

--- make_dataset.py
+++ make_dataset.py
@@ -1,14 +1,11 @@
-"""
-Reads in raw text datasets turning them into lists of indices for use in training the model.
-Expects text of the form
-word==##==tag
-word==##==tag
-...
-===== (separate sentences with this token)
-word/tag
-word/tag
-...
+# TODO right now there is kinda a chicken and egg problem.
+# this file outputs the vocab list which would be used to get the morpheme segmentations list
+# could just use Morfessor but that would require dependence on Morfessor and might be obnoxious to change
+# later
+# Probably just add a flag like --output-vocab-file that will just dump the words and exit
 
+"""
+Reads in CONLL files to make the dataset
 Output a cPickle file of a dict with the following elements
 training_instances: List of (sentence, tags) for training data
 dev_instances
@@ -22,8 +19,23 @@ import codecs
 import argparse
 import cPickle
 import collections
+import morfessor
 
 Instance = collections.namedtuple("Instance", ["sentence", "tags"])
+
+def read_morpheme_segmentations(filename, w2i, m2i):
+    segmentations = {}
+    with codecs.open(filename, "r", "utf-8") as f:
+        for line in f:
+            split = line.rstrip().lstrip().split()
+            word = split[0]
+            morphemes = split[1:]
+            for m in morphemes:
+                if m not in m2i:
+                    m2i[m] = len(m2i)
+            segmentations[w2i[word]] = [ m2i[m] for m in morphemes ]
+    return segmentations
+
 
 def read_file(filename, w2i, t2i):
     """
@@ -31,45 +43,58 @@ def read_file(filename, w2i, t2i):
     Modifies the w2i and t2i dicts, adding new words as it sees them.
     """
     instances = []
+    vocab_counter = collections.Counter()
     with codecs.open(filename, "r", "utf-8") as f:
         sentence = []
-        tags     = []
+        tags = []
         for i, line in enumerate(f):
-            line = line.rstrip().lstrip()
-            if line == "=====":
+            if line.startswith("#"):
+                continue # Some files like Italian have comments
+            elif line.isspace():
+                # Reached the end of a sentence
                 instances.append(Instance(sentence, tags))
                 sentence = []
-                tags     = []
+                tags = []
             else:
-                split     = line.split("==##==")
-                word, tag = split
-                word      = word.lower()
+                data = line.split("\t")
+                if '-' in data[0]: # Italian has contractions on a separate line, we don't want to include them also
+                    continue
+                word = data[1]
+                tag = data[3] if options.ud_tags else data[4]
+                vocab_counter[word] += 1
                 if word not in w2i:
                     w2i[word] = len(w2i)
                 if tag not in t2i:
                     t2i[tag] = len(t2i)
                 sentence.append(w2i[word])
                 tags.append(t2i[tag])
-    return instances
+    return instances, vocab_counter
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--training-data", required=True, dest="training_data", help="Training data .txt file")
 parser.add_argument("--dev-data", required=True, dest="dev_data", help="Development data .txt file")
 parser.add_argument("--test-data", required=True, dest="test_data", help="Test data .txt file")
+parser.add_argument("--ud-tags", dest="ud_tags", action="store_true", help="Extract UD tags instead of original tags")
+parser.add_argument("--morpheme-segmentations", dest="morpheme_segmentations", help="Morpheme segmentations file")
 parser.add_argument("-o", required=True, dest="output", help="Output filename (.pkl)")
 parser.add_argument("--vocab-file", dest="vocab_file", default="vocab.txt", help="Text file containing all of the words in \
                     the train/dev/test data to use in outputting embeddings")
 options = parser.parse_args()
 
-w2i    = {}
-t2i    = {}
+
+w2i = {}
+t2i = {}
 output = {}
-output["training_instances"] = read_file(options.training_data, w2i, t2i)
-output["dev_instances"]      = read_file(options.dev_data, w2i, t2i)
-output["test_instances"]     = read_file(options.test_data, w2i, t2i)
-output["w2i"]                = w2i
-output["t2i"]                = t2i
+output["training_instances"], output["training_vocab"] = read_file(options.training_data, w2i, t2i)
+output["dev_instances"], output["dev_vocab"] = read_file(options.dev_data, w2i, t2i)
+output["test_instances"], output["test_vocab"] = read_file(options.test_data, w2i, t2i)
+if options.morpheme_segmentations is not None:
+    m2i = {}
+    output["morpheme_segmentations"] = read_morpheme_segmentations(options.morpheme_segmentations, w2i, m2i)
+    output["m2i"] = m2i
+output["w2i"] = w2i
+output["t2i"] = t2i
 
 with open(options.output, "w") as outfile:
     cPickle.dump(output, outfile)
