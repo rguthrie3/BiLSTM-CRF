Fixed forward algorithm, stopped trying to do it as a matrix computation so its slow (getting the hang of dynet)

model.py
@@ -16,47 +16,99 @@ Instance = collections.namedtuple("Instance", ["sentence", "tags"])
 
 class BiLSTM_CRF:
 
-    def __init__(self, vocab_size, tagset_size, num_lstm_layers, embeddings, hidden_dim):
+    def __init__(self, tagset_size, num_lstm_layers, hidden_dim, word_embeddings, morpheme_embeddings, morpheme_projection, morpheme_decomps, train_vocab_ctr):
         self.model = dy.Model()
         self.tagset_size = tagset_size
-        embedding_dim = embeddings.shape[1]
+        self.train_vocab_ctr = train_vocab_ctr
 
         # Word embedding parameters
-        self.words_lookup = self.model.add_lookup_parameters((vocab_size, embedding_dim))
-        self.words_lookup.init_from_array(embeddings)
+        vocab_size = word_embeddings.shape[0]
+        word_embedding_dim = word_embeddings.shape[1]
+        self.words_lookup = self.model.add_lookup_parameters((vocab_size, word_embedding_dim))
+        self.words_lookup.init_from_array(word_embeddings)
+
+        # Morpheme embedding parameters
+        # morpheme_vocab_size = morpheme_embeddings.shape[0]
+        # morpheme_embedding_dim = morpheme_embeddings.shape[1]
+        # self.morpheme_lookup = self.model.add_lookup_parameters((morpheme_vocab_size, morpheme_embedding_dim))
+        # self.morpheme_lookup.init_from_array(morpheme_embeddings)
+        # self.morpheme_decomps = morpheme_decomps
+
+        # if morpheme_projection is not None:
+        #     self.morpheme_projection = self.model.add_parameters((word_embedding_dim, morpheme_embedding_dim))
+        #     self.morpheme_projection.init_from_array(morpheme_projection)
+        # else:
+        #     self.morpheme_projection = None
 
         # LSTM parameters
-        self.bi_lstm = dy.BiRNNBuilder(num_lstm_layers, embedding_dim, hidden_dim, self.model, dy.LSTMBuilder)
+        self.bi_lstm = dy.BiRNNBuilder(num_lstm_layers, word_embedding_dim, hidden_dim, self.model, dy.LSTMBuilder)
         
         # Matrix that maps from Bi-LSTM output to num tags
         self.lstm_to_tags_params = self.model.add_parameters((tagset_size, hidden_dim))
+        self.mlp_out = self.model.add_parameters((tagset_size, tagset_size))
 
         # Transition matrix for tagging layer, [i,j] is score of transitioning to i from j
         self.transitions = self.model.add_lookup_parameters((tagset_size, tagset_size))
 
 
+    def set_dropout(self, p):
+        self.bi_lstm.set_dropout(p)
+
+
+    def disable_dropout(self):
+        self.bi_lstm.disable_dropout()
+
+
+    def word_rep(self, word):
+        """
+        For rare words in the training data, we will use their morphemes
+        to make their representation
+        """ 
+        if self.train_vocab_ctr[word] > 5:
+            return self.words_lookup[word]
+        else:
+            # Use morpheme embeddings
+            morpheme_decomp = self.morpheme_decomps[word]
+            rep = self.morpheme_lookup[morpheme_decomp[0]]
+            for m in morpheme_decomp[1:]:
+                rep += self.morpheme_lookup[m]
+            if self.morpheme_projection is not None:
+                rep = self.morpheme_projection * rep
+            if np.linalg.norm(rep.npvalue()) >= 50.0:
+                # This is meant to handle things like URLs and weird tokens like !!!!!!!!!!!!!!!!!!!!!
+                # that are splitting into a lot of morphemes, and their large representations are cause NaNs
+                # TODO handle this in a better way.  Looks like all such inputs are either URLs, email addresses, or
+                # long strings of a punctuation token when the decomposition is > 10
+                return self.words_lookup[w2i["<UNK>"]]
+            return rep
+
+
     def build_tagging_graph(self, sentence):
         dy.renew_cg()
 
+        #embeddings = [self.word_rep(w) for w in sentence]
         embeddings = [self.words_lookup[w] for w in sentence]
         lstm_out = self.bi_lstm.transduce(embeddings)
         
         H = dy.parameter(self.lstm_to_tags_params)
-        probs = []
+        O = dy.parameter(self.mlp_out)
+        scores = []
         for rep in lstm_out:
-            prob_t = dy.log_softmax(H * rep)
-            probs.append(prob_t)
+            score_t = O * dy.tanh(H * rep)
+            scores.append(score_t)
 
-        return probs
+        return scores
 
 
     def score_sentence(self, observations, tags):
         assert len(observations) == len(tags)
+        score_seq = [0]
         score = dy.scalarInput(0)
-        tags = [self.tagset_size - 2] + tags
+        tags = [t2i["<START>"]] + tags
         for i, obs in enumerate(observations):
-            score = score + dy.pick(self.transitions[tags[i+1]], tags[i]) + obs[tags[i+1]]
-        score = score + dy.pick(self.transitions[self.tagset_size - 1], tags[-1])
+            score = score + dy.pick(self.transitions[tags[i+1]], tags[i]) + dy.pick(obs, tags[i+1])
+            score_seq.append(score.value())
+        score = score + dy.pick(self.transitions[t2i["<STOP>"]], tags[-1])
         return score
 
 
@@ -72,46 +124,39 @@ class BiLSTM_CRF:
 
     def neg_log_loss(self, sentence, tags):
         observations = self.build_tagging_graph(sentence)
-        forward_score = self.forward(observations)
         gold_score = self.score_sentence(observations, tags)
-        return -(gold_score - forward_score)
+        forward_score = self.forward(observations)
+        return forward_score - gold_score
 
 
     def forward(self, observations):
 
         def log_sum_exp(scores):
-            # I implemented it like in this implementation
-            # https://github.com/glample/tagger/blob/master/nn.py
-            scores_np            = scores.npvalue()
-            max_score            = np.max(scores_np, axis=0)
-            max_score_broadcast  = max_score.repeat(self.tagset_size).reshape(self.tagset_size, self.tagset_size).T
-            max_score_expr       = dy.inputVector(max_score)
-            max_score_bcast_expr = dy.inputMatrix(max_score_broadcast.flatten(), (self.tagset_size, self.tagset_size))
-            return max_score_expr + dy.log(dy.sum_cols(dy.transpose(dy.exp(scores - max_score_bcast_expr))))
+            npval = scores.npvalue()
+            argmax_score = np.argmax(npval)
+            max_score_expr = dy.pick(scores, argmax_score)
+            max_score_expr_broadcast = dy.concatenate([max_score_expr] * self.tagset_size)
+            return max_score_expr + dy.log(dy.sum_cols(dy.transpose(dy.exp(scores - max_score_expr_broadcast))))
 
-        init_alphas     = [-1e10] * self.tagset_size
-        init_alphas[-2] = 0
-        for_expr        = dy.inputVector(init_alphas)
-        trans_matrix    = dy.concatenate_cols([self.transitions[idx] for idx in xrange(self.tagset_size)])
+        init_alphas = [-1e10] * self.tagset_size
+        init_alphas[t2i["<START>"]] = 0
+        for_expr = dy.inputVector(init_alphas)
         for i, obs in enumerate(observations):
-            obs_matrix  = dy.transpose(dy.concatenate_cols([obs] * self.tagset_size))
-            prev_matrix = dy.concatenate_cols([for_expr] * self.tagset_size)
-            scores      = obs_matrix + prev_matrix + trans_matrix
-            for_expr    = log_sum_exp(scores)
-        terminal_expr  = for_expr + self.transitions[self.tagset_size - 1]
-        terminal_np    = terminal_expr.npvalue()
-        terminal_max   = np.max(terminal_np)
-        max_expr       = dy.scalarInput(terminal_max)
-        max_bcast      = terminal_max.repeat(self.tagset_size).reshape(self.tagset_size)
-        max_bcast_expr = dy.inputVector(max_bcast)
-        alpha         = max_expr + dy.log(dy.sum_cols(dy.transpose(dy.exp(terminal_expr - max_bcast_expr))))
+            alphas_t = []
+            for next_tag in range(self.tagset_size):
+                obs_broadcast = dy.concatenate([dy.pick(obs, next_tag)] * self.tagset_size)
+                next_tag_expr = for_expr + self.transitions[next_tag] + obs_broadcast
+                alphas_t.append(log_sum_exp(next_tag_expr))
+            for_expr = dy.concatenate(alphas_t)
+        terminal_expr = for_expr + self.transitions[t2i["<STOP>"]]
+        alpha = log_sum_exp(terminal_expr)
         return alpha
 
 
     def viterbi_decoding(self, observations):
         backpointers = []
         init_vvars   = [-1e10] * self.tagset_size
-        init_vvars[-2] = 0 # <Start> has all the probability
+        init_vvars[t2i["<START>"]] = 0 # <Start> has all the probability
         for_expr     = dy.inputVector(init_vvars)
         trans_exprs  = [self.transitions[idx] for idx in range(self.tagset_size)]
         for i, obs in enumerate(observations):
@@ -126,7 +171,7 @@ class BiLSTM_CRF:
             for_expr = dy.concatenate(vvars_t) + obs
             backpointers.append(bptrs_t)
         # Perform final transition to terminal
-        terminal_expr = for_expr + trans_exprs[-1]
+        terminal_expr = for_expr + trans_exprs[t2i["<STOP>"]]
         terminal_arr  = terminal_expr.npvalue()
         best_tag_id   = np.argmax(terminal_arr)
         path_score    = dy.pick(terminal_expr, best_tag_id)
@@ -151,12 +196,15 @@ class BiLSTM_CRF:
 # ===-----------------------------------------------------------------------===
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", required=True, dest="dataset", help=".pkl file to use")
-parser.add_argument("--embeddings", required=True, dest="embeddings", help="File from which to read in pretrained embeds")
-parser.add_argument("--num-epochs", default=15, dest="num_epochs", help="Number of full passes through training set")
+parser.add_argument("--word-embeddings", required=True, dest="word_embeddings", help="File from which to read in pretrained embeds")
+parser.add_argument("--morpheme-embeddings", dest="morpheme_embeddings", help="File from which to read in pretrained embeds")
+parser.add_argument("--morpheme-projection", dest="morpheme_projection", help="Pickle file containing projection matrix if applicable")
+parser.add_argument("--num-epochs", default=50, dest="num_epochs", help="Number of full passes through training set")
 parser.add_argument("--lstm-layers", default=2, dest="lstm_layers", help="Number of LSTM layers")
 parser.add_argument("--hidden-dim", default=128, dest="hidden_dim", help="Size of LSTM hidden layers")
 parser.add_argument("--learning-rate", default=0.001, dest="learning_rate", help="Initial learning rate")
 parser.add_argument("--dropout", default=-1, dest="dropout", help="Amount of dropout to apply to LSTM part of graph")
+parser.add_argument("--viterbi", dest="viterbi", action="store_true", help="Use viterbi training instead of CRF")
 parser.add_argument("--log-dir", default="log", dest="log_dir", help="Directory where to write logs / serialized models")
 options = parser.parse_args()
 
@@ -180,58 +228,111 @@ train_dev_cost = utils.CSVLogger(options.log_dir + "/train_dev.log", ["Train.cos
 # ===-----------------------------------------------------------------------===
 dataset = cPickle.load(open(options.dataset, "r"))
 w2i = dataset["w2i"]
+w2i["<UNK>"] = len(w2i) # read the comment in word_rep to see why this is necessary
 t2i = dataset["t2i"]
+#m2i = dataset["m2i"]
+m2i = None
 t2i["<START>"] = len(t2i)
 t2i["<STOP>"] = len(t2i)
 i2w = { i: w for w, i in w2i.items() } # Inverse mapping
 i2t = { i: t for t, i in t2i.items() }
 tag_list = [ i2t[idx] for idx in xrange(len(i2t)) ] # To use in the confusion matrix
 training_instances = dataset["training_instances"]
-dev_instances      = dataset["dev_instances"]
-test_instances     = dataset["test_instances"]
+training_vocab = dataset["training_vocab"]
+dev_instances = dataset["dev_instances"]
+dev_vocab = dataset["dev_vocab"]
 
 
 # ===-----------------------------------------------------------------------===
 # Build model and trainer
 # ===-----------------------------------------------------------------------===
-embeddings = utils.read_pretrained_embeddings(options.embeddings, w2i)
-bilstm_crf = BiLSTM_CRF(len(w2i), len(t2i), options.lstm_layers, embeddings, options.hidden_dim)
-trainer    = dy.AdamTrainer(bilstm_crf.model, options.learning_rate)
+word_embeddings = utils.read_pretrained_embeddings(options.word_embeddings, w2i)
+#morpheme_embeddings = utils.read_pretrained_embeddings(options.morpheme_embeddings, m2i)
+# if options.morpheme_projection is not None:
+#     assert word_embeddings.shape[1] != morpheme_embeddings.shape[1]
+#     morpheme_projection = cPickle.load(open(options.morpheme_projection, "r"))
+# else:
+#     morpheme_projection = None
+
+morpheme_embeddings = None
+morpheme_projection = None
+morpheme_decomps = None
+#morpheme_decomps = dataset["morpheme_segmentations"]
+bilstm_crf = BiLSTM_CRF(len(t2i), int(options.lstm_layers), options.hidden_dim, word_embeddings, morpheme_embeddings, morpheme_projection, morpheme_decomps, training_vocab)
+trainer = dy.AdamTrainer(bilstm_crf.model, options.learning_rate)
+
+print "Number training instances:", len(training_instances)
+print "Number Dev instances:", len(dev_instances)
 
 for epoch in xrange(options.num_epochs):
     bar = progressbar.ProgressBar()
     random.shuffle(training_instances)
     train_loss = 0.0
+    train_correct = 0
+    train_total = 0
+    if options.dropout > 0:
+        bilstm_crf.set_dropout(float(options.dropout))
     for instance in bar(training_instances):
-        loss_expr, viterbi_tags = bilstm_crf.viterbi_loss(instance.sentence, instance.tags)
-        loss = loss_expr.scalar_value()
+        if options.viterbi:
+            loss_expr, viterbi_tags = bilstm_crf.viterbi_loss(instance.sentence, instance.tags)
+            loss = loss_expr.scalar_value()
+            # Record some info for training accuracy
+            if loss > 0:
+                for gold, viterbi in zip(instance.tags, viterbi_tags):
+                    if gold == viterbi:
+                        train_correct += 1
+            else:
+                train_correct += len(instance.tags)
+            train_total += len(instance.tags)
+        else:
+            loss_expr = bilstm_crf.neg_log_loss(instance.sentence, instance.tags)
+            loss = loss_expr.scalar_value()
+
+        # Bail if loss is NaN
         if math.isnan(loss):
-            assert False
+            assert False, "NaN occured"
+
         train_loss += (loss / len(instance.sentence))
+
+        # Do backward pass and update parameters
         loss_expr.backward()
         trainer.update()
+
+
+    logging.info("\n")
     logging.info("Epoch {} complete".format(epoch + 1))
+    trainer.update_epoch(1)
 
     # Evaluate dev data
-    logging.info("Evaluating on dev data...")
-    bar = progressbar.ProgressBar()
-    cm = utils.ConfusionMatrix(tag_list)
+    bilstm_crf.disable_dropout()
     dev_loss = 0.0
-    random.shuffle(dev_instances)
-    all_gold = []
-    all_viterbi = []
+    dev_correct = 0
+    dev_total = 0
+    bar = progressbar.ProgressBar()
+    total_wrong = 0
+    total_wrong_oov = 0
     for instance in bar(dev_instances):
+        loss = bilstm_crf.neg_log_loss(instance.sentence, instance.tags)
         viterbi_loss, viterbi_tags = bilstm_crf.viterbi_loss(instance.sentence, instance.tags)
-        dev_loss += (viterbi_loss.value() / len(instance.sentence))
-        all_gold += instance.tags
-        all_viterbi += viterbi_tags
-    correct_tags = 0
-    for gold, viterbi in zip(all_gold, all_viterbi):
-        if gold == viterbi:
-            correct_tags += 1
-    logging.info("Accuracy: {}".format(float(correct_tags) / len(all_gold)))
-    cm.add(all_gold, all_viterbi)
+        dev_loss += (loss.value() / len(instance.sentence))
+        for i, (gold, viterbi) in enumerate(zip(instance.tags, viterbi_tags)):
+            if gold == viterbi:
+                dev_correct += 1
+            else:
+                # Got the wrong tag
+                total_wrong += 1
+                if i2w[instance.sentence[i]] not in training_vocab:
+                    total_wrong_oov += 1
+                
+        dev_total += len(instance.tags)
+    if options.viterbi:
+        logging.info("Train Accuracy: {}".format(float(train_correct) / train_total))
+    logging.info("Dev Accuracy: {}".format(float(dev_correct) / dev_total))
+    if total_wrong > 0:
+        logging.info("% Wrong that are OOV: {}".format(float(total_wrong_oov) / total_wrong))
+
     train_loss = train_loss / len(training_instances)
     dev_loss = dev_loss / len(dev_instances)
-    train_dev_cost.add_column([str(train_loss), str(dev_loss)])
-    cm.plot()
+    logging.info("Train Loss: {}".format(train_loss))
+    logging.info("Dev Loss: {}".format(dev_loss))
+    train_dev_cost.add_column([train_loss, dev_loss])