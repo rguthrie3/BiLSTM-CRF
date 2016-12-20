77a868b647d995fe227047f1d341127650ecb15b
Yuval Pinter <ypinter3@gatech.edu>
2016-12-20 12:53:46 -0500

    celex script and doc

---
+++ scripts/celex-join.py
@@ -0,0 +1,12 @@
+import sys
+
+out = open("joined.txt", "w")
+lems = open(sys.argv[1], "r")
+segs = open(sys.argv[2], "r")
+for l,s in zip(lems.readlines(), segs.readlines()):
+    l = l.strip()
+    s = s.strip()
+    if l == s: continue
+    if len(l) == 0 or len(s) == 0: continue
+    out.write(l + " " + " ".join(s.split("+")) + "\n")
+out.close()
