f145ffe7bad4aa494f2c5139f852007359e4e587
Yuval Pinter <uvp@gatech.edu>
2017-01-25 20:27:35 -0500

    att-align works, maybe needs debug

--- att_align.py
+++ att_align.py
@@ -1,18 +1,16 @@
 from __future__ import division
 from collections import Counter, defaultdict
+from codecs import open
+from evaluate_morphotags import Evaluator
+from utils import split_tagstring
 
 import collections
 import argparse
 import random
 import cPickle
-import logging
-import progressbar
 import os
 import math
 import numpy as np
-
-import utils
-
 
 Instance = collections.namedtuple("Instance", ["sentence", "tags"])
 
@@ -54,12 +52,65 @@ for inst in train:
 
 print pos_atts
 
-# load dev file
+# init F1 calculators
+curr_eval = Evaluator()
+dict_removed_eval = Evaluator()
+oracle_eval = Evaluator()
+posonly_eval = Evaluator()
 
-# load dev from pos_only
-
+# load dev files
 # calculate micro F1 for:
 # current (=validation)
 # with pos_atts data from dev POS (remove unknowns)
 # with pos_atts data from pos-only dev POS (usually better -> pipeline)
 # with pos_atts data from gold POS (oracle)
+valid = 0
+joint_good = 0
+posonly_good = 0
+dev_pairs_not_in_train = 0
+with open(options.results, "r", encoding="utf-8") as dev_results:
+    with open(options.pos_results, "r", encoding="utf-8") as pos_dev_results:
+        for res, pos_res in zip(dev_results.readlines(), pos_dev_results.readlines()):
+            res_cols = res.split("\t")
+            if len(res_cols) < 4: continue
+            pos_res_cols = pos_res.split("\t")
+            assert res_cols[0:1] == pos_res_cols[0:1]
+            gold = split_tagstring(res_cols[2])
+            curr_obs = split_tagstring(res_cols[4])
+            dict_removed_obs = {}
+            posonly_obs = {}
+            oracle_obs = {}
+            dict_allow_atts = pos_atts[res_cols[3]]
+            posonly_atts = pos_atts[pos_res_cols[3]]
+            oracle_atts = pos_atts[res_cols[1]]
+            if res_cols[1] == res_cols[3]:
+                joint_good += 1
+            if res_cols[1] == pos_res_cols[3]:
+                posonly_good += 1
+            # populate obs-s (TODO: list-compify)
+            for att,val in curr_obs.iteritems():
+                if att in dict_allow_atts:
+                    dict_removed_obs[att] = val
+                if att in posonly_atts:
+                    posonly_obs[att] = val
+                if att in oracle_atts:
+                    oracle_obs[att] = val
+            for a,v in gold.iteritems():
+                if a not in oracle_atts:
+                    dev_pairs_not_in_train += 1
+            
+            # add to evaluators
+            curr_eval.add_instance(gold, curr_obs)
+            dict_removed_eval.add_instance(gold, dict_removed_obs)
+            oracle_eval.add_instance(gold, oracle_obs)
+            posonly_eval.add_instance(gold, posonly_obs)
+            valid += 1
+
+print valid
+print curr_eval.mic_f1()
+print dict_removed_eval.mic_f1()
+print oracle_eval.mic_f1()
+print posonly_eval.mic_f1()
+print dev_pairs_not_in_train
+print joint_good / valid
+print posonly_good / valid
