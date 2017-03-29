9da4b8e218bcfeaa8715c1ec1a5ec3a66db59035
Yuval Pinter <uvp@gatech.edu>
2017-03-29 14:09:26 -0400

    oov extraction

--- token-size-exp/extract.py
+++ token-size-exp/extract.py
@@ -18,13 +18,26 @@ def dev_mic_att_acc(line_batch):
 def test_mic_att_acc(line_batch):
     return float(line_batch[4].strip().split()[3])
 
-langs = ['da','en','hu','it','lv','tr','vi']
+def dev_pos_oov_acc(line_batch):
+    return float(line_batch[5].strip().split()[-1])
+
+def test_pos_oov_acc(line_batch):
+    return float(line_batch[6].strip().split()[-1])
+
+# langs = ['da', 'en', 'hu', 'it', 'lv', 'tr', 'vi']
+# langs = ['ta', 'fa', 'ru', 'sv', 'he', 'bg', 'hi', 'cs', 'es']
+langs = ['ta', 'fa', 'ru', 'sv', 'he', 'bg']
 models = ['nochar','tagchar','mchar','bothchar']
-with open('pg-summary.txt','w') as sum_file:
+
+#oov = True
+
+with open('pg-summary{}-{}.txt'.format("-oov" if oov else "", "-".join(langs)),'w') as sum_file:
     for lg in langs:
         dev_pos_acc_averages = {}
+        dev_pos_oov_acc_averages = {}
         dev_att_f1_averages = {}
         dev_pos_acc_stddevs = {}
+        dev_pos_oov_acc_stddevs = {}
         dev_att_f1_stddevs = {}
         for m in models:
             with open('results-{}-pg-{}.txt'.format(lg, m), 'r') as res_file:
@@ -34,16 +47,20 @@ with open('pg-summary.txt','w') as sum_file:
                 # init file-through reporting vars
                 tr_tok_headers = []
                 dev_pos_acc_averages[m] = []
+                dev_pos_oov_acc_averages[m] = []
                 dev_att_f1_averages[m] = []
                 dev_pos_acc_stddevs[m] = []
+                dev_pos_oov_acc_stddevs[m] = []
                 dev_att_f1_stddevs[m] = []
                 
                 # init per-setup vars
                 tr_toks = 0
                 run = 0
                 dev_pos_accs = []
+                dev_pos_oov_accs = []
                 dev_att_f1s = []
-                # TODO add more (test, OOV)
+                # TODO add test
+                
                 for b in res_batches:
                     tl = title_line(b)
                     assert tl.startswith('.')
@@ -55,22 +72,32 @@ with open('pg-summary.txt','w') as sum_file:
                     else:
                         tr_toks = curr_tr_toks
                     dev_pos_accs.append(dev_pos_acc(b))
+                    dev_pos_oov_accs.append(dev_pos_oov_acc(b))
                     dev_att_f1s.append(dev_mic_att_acc(b))
                     if curr_part == 5:
+                    
                         # add accumlations to reporting
                         tr_tok_headers.append(str(tr_toks))
                         dev_pos_acc_averages[m].append("{:4f}".format(np.average(dev_pos_accs)))
+                        dev_pos_oov_acc_averages[m].append("{:4f}".format(np.average(dev_pos_oov_accs)))
                         dev_att_f1_averages[m].append("{:4f}".format(np.average(dev_att_f1s)))
+                        
                         dev_pos_acc_stddevs[m].append("{:4f}".format(np.std(dev_pos_accs)))
+                        dev_pos_oov_acc_stddevs[m].append("{:4f}".format(np.std(dev_pos_oov_accs)))
                         dev_att_f1_stddevs[m].append("{:4f}".format(np.std(dev_att_f1s)))
                         
                         # init per-setup vars
                         dev_pos_accs = []
+                        dev_pos_oov_accs = []
                         dev_att_f1s = []
                         
         # write all reporting in output file
         sum_file.write("{}-polyglot:\n".format(lg))
-        for dict, name in zip([dev_pos_acc_averages, dev_att_f1_averages, dev_pos_acc_stddevs, dev_att_f1_stddevs], ['POS-acc-avg','Att-F1-avg','POS-acc-std','Att-F1-std']):
+        if oov:
+            fields = zip([dev_pos_oov_acc_averages], ['POS-acc-oov-avg'])
+        else:
+            fields = zip([dev_pos_acc_averages, dev_att_f1_averages, dev_pos_acc_stddevs, dev_att_f1_stddevs], ['POS-acc-avg','Att-F1-avg','POS-acc-std','Att-F1-std'])
+        for dict, name in fields:
             sum_file.write(name + '\t' + '\t'.join(tr_tok_headers) + '\n')
             for m in models:
                 sum_file.write(m + '\t' + '\t'.join(dict[m]) + '\n')
