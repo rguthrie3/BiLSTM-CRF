d2bb319a40f8614623b2843a2d1c21320829355f
Yuval Pinter <uvp@gatech.edu>
2017-04-14 10:23:56 -0400

    significance now outputs in paper format

--- scripts/significance.py
+++ scripts/significance.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from codecs import open
 from utils import split_tagstring
 from evaluate_morphotags import Evaluator
@@ -35,6 +36,15 @@ def extract_att_stats(file1, file2, n):
     all_pairs = [(gold_atts(l1.split('\t')), pred_atts(l1.split('\t')), pred_atts(l2.split('\t')))\
                     for l1, l2 in zip(file1, file2) if len(l1.strip()) > 0]
 
+    # normal f1 for reporting
+    basef1_eval1 = Evaluator(m = 'att')
+    basef1_eval2 = Evaluator(m = 'att')
+    for ga, oa1, oa2 in all_pairs:
+        basef1_eval1.add_instance(ga, oa1)
+        basef1_eval2.add_instance(ga, oa2)
+    f11 = basef1_eval1.mic_f1() * 100
+    f12 = basef1_eval2.mic_f1() * 100
+
     # retrieve samples based on this randomization
     boot_assignments = bootstrap_samples(len(all_pairs), n)
 
@@ -52,7 +62,7 @@ def extract_att_stats(file1, file2, n):
     diff_arr = np.array(f1_diffs)
 
     # compute p-value
-    return scipy.stats.norm.cdf(diff_arr.mean() / diff_arr.std())
+    return f11, f12, scipy.stats.norm.cdf(diff_arr.mean() / diff_arr.std())
 
 ### POS tagging - Wilcoxon ###
 
@@ -64,6 +74,8 @@ def extract_pos_stats(file1, file2):
     wrong2 = 0
     wrong_oov1 = 0
     wrong_oov2 = 0
+    corr1 = 0
+    corr2 = 0
     total = 0
     for l1, l2 in zip(file1, file2):
         if len(l1.strip()) == 0:
@@ -82,6 +94,12 @@ def extract_pos_stats(file1, file2):
 
         ppred1 = pred_pos(cols1)
         ppred2 = pred_pos(cols2)
+
+        #accuracy in percent
+        if ppred1 == pgold: corr1 += 100
+        if ppred2 == pgold: corr2 += 100
+
+        #significance
         if ppred1 == ppred2: continue
 
         if ppred1 != pgold and ppred2 == pgold:
@@ -93,32 +111,53 @@ def extract_pos_stats(file1, file2):
             if isoov:
                 wrong_oov2 += 1
 
-    return wrong1, wrong2, total, wil_p(wrong1, wrong2),\
+    return corr1/total, corr2/total, wrong1, wrong2, total, wil_p(wrong1, wrong2),\
             wrong_oov1, wrong_oov2, wil_p(wrong_oov1, wrong_oov2)
 
-debug = True
-langs = ['fa', 'hi', 'en', 'es', 'it', 'da', 'he', 'sv', 'bg', 'cs', 'lv', 'hu', 'tr', 'ta', 'ru', 'vi']
-base_format = "logs_token_exp_sign/log-{}-10k-noseq-pginit-{}char-05dr/testout.txt"
-#bar = 0.01
-bar = 0.05
+debug = False
+# langs = ['fa', 'hi', 'en', 'es', 'it', 'da', 'he', 'sv', 'bg', 'cs', 'lv', 'hu', 'tr', 'ta', 'ru', 'vi'] # order by %OOV
+langs = ['ta', 'lv', 'vi', 'hu', 'tr', 'bg', 'sv', 'ru', 'da', 'fa', 'he', 'en', 'hi', 'it', 'es', 'cs'] # order by unlabeled data
+base_format = "rerun_full_logs/log-{}-rerun-noseq-pginit-{}char-05dr/testout.txt"
+#base_format = "logs_token_exp_sign/log-{}-10k-noseq-pginit-{}char-05dr/testout.txt"
+bar = 0.01
+#bar = 0.05
 boots_n = 100
+#outtype = 'pos'
+outtype = 'att'
 
-with open("logs_token_exp_10k-sign-{}.txt".format(bar),"w","utf-8") as outfile:
+with open("logs_token_exp_full-sign-{}-{}.txt".format(outtype, bar),"w","utf-8") as outfile:
     for lg in langs:
+        if outtype == 'att' and lg == 'vi': continue
 
         # mchar vs. nochar
         nofile = open(base_format.format(lg, "no"), "r", "utf-8").readlines()
         mfile = open(base_format.format(lg, "m"), "r", "utf-8").readlines()
-        wn, wm, _, wilwo, wno, wmo, wilwov = extract_pos_stats(nofile, mfile)
-        atwo = extract_att_stats(nofile, mfile, boots_n)
+        if outtype == 'pos':
+            accn, accm, wn, wm, _, wilwo, wno, wmo, wilwov = extract_pos_stats(nofile, mfile)
+        else:
+            f1n, f1m, atwo = extract_att_stats(nofile, mfile, boots_n)
+
         if debug: print lg, "w/o", wn, wm, wilwo, wno, wmo, wilwov, atwo
 
         # bothchar vs. tagchar
         tagfile = open(base_format.format(lg, "tag"), "r", "utf-8").readlines()
         bothfile = open(base_format.format(lg, "both"), "r", "utf-8").readlines()
-        wt, wb, _, wilw, wto, wbo, wilwv = extract_pos_stats(tagfile, bothfile)
-        atw = extract_att_stats(tagfile, bothfile, boots_n)
+        if outtype == 'pos':
+            acct, accb, wt, wb, _, wilw, wto, wbo, wilwv = extract_pos_stats(tagfile, bothfile)
+        else:
+            f1t, f1b, atw = extract_att_stats(tagfile, bothfile, boots_n)
+
         if debug: print lg, "with", wt, wb, wilw, wto, wbo, wilwv, atw
 
-        outfile.write("\t".join([lg, str(wilwo < bar), str(wilw < bar),\
-            str(wilwov < bar), str(wilwv < bar), str(atwo < bar), str(atw < bar)]) + "\n")
+        if outtype == 'pos':
+            m_sign = "*" if wilwo < bar else ""
+            b_sign = "*" if wilw < bar else ""
+            outfile.write("{}\t{:.2f}\t{:.2f}\t{:.2f}{}\t{:.2f}{}\n".format(lg,accn,acct,accm,m_sign,accb,b_sign))
+        else:
+            m_sign = "*" if atwo < bar else ""
+            b_sign = "*" if atw < bar else ""
+            outfile.write("{}\t{:.2f}\t{:.2f}\t{:.2f}{}\t{:.2f}{}\n".format(lg,f1n,f1t,f1m,m_sign,f1b,b_sign))
+            print "finished {}".format(lg)
+
+        #outfile.write("\t".join([lg, str(wilwo < bar), str(wilw < bar),\
+            #str(wilwov < bar), str(wilwv < bar), str(atwo < bar), str(atw < bar)]) + "\n")
